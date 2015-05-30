d12333cff411da8f41d0d4f280f47528a2b0fab8
Augusto <infoalquilerventa@gmail.com>
2015-05-30 20:30:16 +0200

    Revisión de Antonio: 	- Librería opencsv no integrada en la carpeta lib del prooyecto, 	solo classpath desde el equipo de María José.
    	- Al añadir archivo, no indicar en la rura "war/...", si estamos
    	desplegando un war, ya estamos ahí.
    	
    	- Al crear la entidad, es necesario llevarla a un método 
    	privado para que el scope del objeto no se exitienda a todo
    	el método (y por tanto, el while).
    	
    	- Hay que tratar las excepciones debidamente para no tener
    	"errores 500" desconocidos.
    	
    	- Siempre tener desplegada una versión funcional y que ésta esté
    	en una rama distinta de GIT, para eso está master.
    	Conviene trabajar con develop y master al menos. Cuesta recuperar
    	un error desde una rama no correcta.
    	
    	- Mucho ánimo, ya queda menos :)

--- SOS-Servlets/src/sos/ServletCSVEnergia.java
+++ SOS-Servlets/src/sos/ServletCSVEnergia.java
@@ -4,7 +4,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,45 +12,65 @@ import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.PreparedQuery.TooManyResultsException;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.opencsv.CSVReader;
 
-@SuppressWarnings("serial")
 public class ServletCSVEnergia extends HttpServlet {
-
+	private static final long serialVersionUID = 1L;
 	DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-	
-	@SuppressWarnings("resource")
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException{
-	
-		CSVReader reader = new CSVReader(new FileReader("war/csv/tiposEnergia.csv"));
-		
-		String [] nextLine;
-		
-		while ((nextLine = reader.readNext()) != null) {
-			
-			Entity entity = new Entity("Energy");
-		
-			entity.setProperty("name", nextLine[0]);
-			entity.setProperty("no_fossil", new Double (nextLine[1]));
-			entity.setProperty("fossil", new Double (nextLine[2]));
-			entity.setProperty("temperature", new Double (nextLine[3]));
-			
-			System.out.println(entity);
-			
-			Query q = new Query ("Energy").setFilter(new FilterPredicate("name", Query.FilterOperator.EQUAL, entity.getProperty("name")));
-			PreparedQuery pq = datastore.prepare(q);
-			Entity en = pq.asSingleEntity();
-			
-			System.out.println(en);
-			
-			if(en != null){
-				datastore.delete(en.getKey());
-				datastore.put(entity);
-			}else{
-				datastore.put(entity);
-			}	
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) {
+		FileReader fr = null;
+		try {
+			fr = new FileReader("csv/tiposEnergia.csv");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
+		CSVReader reader = new CSVReader(fr);
+
+		String[] nextLine;
+		
+
+		try {
+			while ((nextLine = reader.readNext()) != null) {
+				createAndStoreEntity(nextLine[0],new Double(nextLine[1]),new Double(nextLine[2]),new Double(nextLine[3]));
+			}
+		} catch (NumberFormatException | TooManyResultsException | IOException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
-}	
+	
+	private void createAndStoreEntity(String name, Double no_fossil, Double fossil, Double temperature){
+		Entity entity = new Entity("Energy");
+		entity.setProperty("name", name);
+		entity.setProperty("no_fossil", no_fossil);
+		entity.setProperty("fossil", fossil);
+		entity.setProperty("temperature", temperature);
+
+		System.out.println(entity);
+
+		Query q = new Query("Energy").setFilter(new FilterPredicate("name", Query.FilterOperator.EQUAL, entity.getProperty("name")));
+		PreparedQuery pq = datastore.prepare(q);
+		Entity en = pq.asSingleEntity();
+
+		System.out.println(en);
+
+		if (en != null) {
+			datastore.delete(en.getKey());
+			datastore.put(entity);
+		} else {
+			datastore.put(entity);
+		}
+
+		
+	}
+}
