2094ccae39610b04add3b06667114debd402d152
Augusto <infoalquilerventa@gmail.com>
2015-05-31 11:30:30 +0200

    AUGUSTO, va proxy y api externa de papamoscas, que lo consume, por poner orden en codigo. subo esta version en caso de que nos falle algo despues de arreglar esto. animos con la teoria

---
+++ SOS-Servlets/src/sos/PapamoscasProxyServlet.java
@@ -0,0 +1,33 @@
+package sos;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PapamoscasProxyServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 7329647817610291333L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			URL myURL = new URL("http://papamoscas-isa.appspot.com/api/v3/birds?user=proUser1");
+			BufferedReader in = new BufferedReader(new InputStreamReader(myURL.openStream()));
+			 String inputLine;
+			 while ((inputLine = in.readLine()) != null){
+			           //System.out.println(inputLine);
+			           resp.getWriter().append(inputLine);
+			 }
+			 in.close();
+	 
+		} catch (IOException e) {
+				e.printStackTrace();
+		}
+	}
+}
--- SOS-Servlets/war/js/charttiposenergias.js
+++ SOS-Servlets/war/js/charttiposenergias.js
@@ -1,42 +1,45 @@
-google.load('visualization','1.0',{'packages':['corechart']});
-		
-		google.setOnLoadCallback(dibujar);
-		
-		function dibujar()
-		{ 
-			var request = $.ajax({
-				url: "/api/v2/tiposenergias", //http://sos-2015-04.appspot.com
-				type: "GET",
-				dataR: $("#payload").val()
-			});
-			
-			request.done(function(dataR, status, jqXHR){
-					
-				var data = new google.visualization.DataTable();
-				data.addColumn('string','name');
-				data.addColumn('number','fossil');
-				
-				var jsonArray = JSON.parse(dataR);
-				
-				var count;
-				for (count=0;count<jsonArray.length;count++){
-					var name = JSON.stringify(jsonArray[count].name);
-					var fossil = parseFloat(JSON.stringify(jsonArray[count].fossil));
-					data.addRows(
-						[
-							[name,fossil]
-						]
-						);
-					//console.log(name+" - "+temperature+"\n"); en funcion de donde esta me va imprimendo lo que quiero ver....//mas herramientas -> consola JavaScript -> console//
-				}
-				
-				
-				
-				var opciones = {'title':'fossil in Country',
-								'width': 800,
-								'height':600};
-				var grafica = new google.visualization.PieChart(document.getElementById('charts'));
-				grafica.draw(data, opciones);
-			});
-		
+google.load('visualization', '1.0', {
+	'packages' : [ 'corechart' ]
+});
+
+google.setOnLoadCallback(dibujar);
+
+function dibujar() {
+	var request = $.ajax({
+		url : "/api/v2/papamoscas", // http://sos-2015-04.appspot.com
+		type : "GET",
+		dataR : $("#payload").val()
+	});
+
+	request.done(function(dataR, status, jqXHR) {
+
+		var data = new google.visualization.DataTable();
+		data.addColumn('string', 'specie');
+		data.addColumn('number', 'hatches');
+
+		var jsonArray = JSON.parse(dataR);
+
+		var count;
+		for (count = 0; count < jsonArray.length; count++) {
+			var specie = JSON.stringify(jsonArray[count].specie);
+			var hatches = parseFloat(JSON.stringify(jsonArray[count].hatches));
+			data.addRows([ [ specie, hatches ] ]);
+			// console.log(name+" - "+temperature+"\n"); en funcion de donde
+			// esta me va imprimendo lo que quiero ver....//mas herramientas ->
+			// consola JavaScript -> console//
 		}
+
+		var opciones = {
+			'title' : 'fossil in Country',
+			'width' : 800,
+			'height' : 600,
+			backgroundColor : {
+				fill : 'transparent'
+			}
+		};
+		var grafica = new google.visualization.BarChart(document
+				.getElementById('charts'));
+		grafica.draw(data, opciones);
+	});
+
+}
