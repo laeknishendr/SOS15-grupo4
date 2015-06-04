9a5a2d24abb2ed024c9813b864ce453ebf383855
Augusto <infoalquilerventa@gmail.com>
2015-06-04 09:29:55 +0200

    04/06/ MAÑANA DEL SABADO

---
+++ SOS-Servlets/war/js/manu.js
@@ -0,0 +1,45 @@
+google.load('visualization', '1.0', {
+	'packages' : [ 'corechart' ]
+});
+
+google.setOnLoadCallback(dibujar);
+
+function dibujar() {
+	var request = $.ajax({
+		url : "api/v2/manuProxyServlet", // http://sos-2015-04.appspot.com
+		type : "GET",
+		dataR : $("").val()  // #payload --> en este caso al usar el "GET" no  nos hace falta definirlo.
+	});
+
+	request.done(function(dataR, status, jqXHR) {
+
+		var data = new google.visualization.DataTable();
+		data.addColumn('string', 'city');
+		data.addColumn('number', 'year');
+
+		var jsonArray = JSON.parse(dataR);
+
+		var count;
+		for (count = 0; count < jsonArray.length; count++) {
+			var city = JSON.stringify(jsonArray[count].city);
+			var year = parseFloat(JSON.stringify(jsonArray[count].year));
+			data.addRows([ [ city, year ] ]);
+			// console.log(name+" - "+temperature+"\n"); en funcion de donde
+			// esta me va imprimendo lo que quiero ver....//mas herramientas ->
+			// consola JavaScript -> console//
+		}
+
+		var opciones = {
+			'title' : 'Estudiantes',
+			'width' : 800,
+			'height' : 600,
+			backgroundColor : {
+				fill : 'transparent'
+			}
+		};
+		var grafica = new google.visualization.BarChart(document
+				.getElementById('charts'));
+		grafica.draw(data, opciones);
+	});
+
+}
---
+++ SOS-Servlets/war/js/mariajose.js
@@ -0,0 +1,53 @@
+google.load('visualization', '1.0', {
+	'packages' : [ 'corechart' ]
+});
+
+google.setOnLoadCallback(dibujar);
+
+function dibujar() {
+	var request = $.ajax({
+		url : "api/v2/mariajoseProxyServlet", // http://sos-2015-04.appspot.com
+		type : "GET",
+		dataR : $("").val()  // #payload --> en este caso al usar el "GET" no  nos hace falta definirlo.
+	});
+
+	request.done(function(dataR, status, jqXHR) {
+
+		var data = new google.visualization.DataTable();
+		data.addColumn('string', 'name');
+		data.addColumn('number', 'total_population');
+		
+		//console.log(dataR);
+		
+		var aux = JSON.parse(dataR);
+		
+		//console.log(aux);
+		
+		var jsonArray = aux.results;
+		
+		//console.log(jsonArray);
+		
+		var count;
+		for (count = 0; count < jsonArray.length; count++) {
+			var name = JSON.stringify(jsonArray[count].name);
+			var total_population = parseFloat(JSON.stringify(jsonArray[count].total_population));
+			data.addRows([ [ name, total_population ] ]);
+			// console.log(name+" - "+temperature+"\n"); en funcion de donde
+			// esta me va imprimendo lo que quiero ver....//mas herramientas ->
+			// consola JavaScript -> console//
+		}
+
+		var opciones = {
+			'title' : 'public accounts',
+			'width' : 1600,
+			'height' : 1200,
+			backgroundColor : {
+				fill : 'transparent'
+			}
+		};
+		var grafica = new google.visualization.BarChart(document
+				.getElementById('charts'));
+		grafica.draw(data, opciones);
+	});
+
+}
